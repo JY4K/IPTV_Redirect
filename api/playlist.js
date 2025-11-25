c0e58dfdbb36a108d476bbb089e9021c20f624c8
白猫 <jy4k.api@gmail.com>
2025-11-25 19:30:24 +0800

    优化

--- api/playlist.js
+++ api/playlist.js
@@ -1,71 +1,43 @@
 const fs = require('fs');
 const path = require('path');
 
-function loadChannels() {
-  const jsonPath = path.join(process.cwd(), 'data', 'channels.json');
-  try {
-    return JSON.parse(fs.readFileSync(jsonPath, 'utf8'));
-  } catch (e) {
-    return [];
-  }
-}
-
 export default function handler(req, res) {
   const { type } = req.query;
+  const baseUrl = `${req.headers['x-forwarded-proto'] || 'http'}://${req.headers.host}`;
+  const iptvUrl = `${baseUrl}/iptv.php?id=`;
   
-  const protocol = req.headers['x-forwarded-proto'] || 'http';
-  const host = req.headers['x-forwarded-host'] || req.headers.host;
-  const baseUrl = `${protocol}://${host}`;
-  const iptvBaseUrl = `${baseUrl}/iptv.php?id=`;
-
-  const data = loadChannels();
+  let data = [];
+  try {
+    data = JSON.parse(fs.readFileSync(path.join(process.cwd(), 'data', 'channels.json'), 'utf8'));
+  } catch { }
 
   if (type === 'm3u') {
-    let m3uContent = '#EXTM3U x-tvg-url="https://fy.188766.xyz/e.xml"\n';
-
+    let content = '#EXTM3U x-tvg-url="https://fy.188766.xyz/e.xml"\n';
     data.forEach(group => {
-      group.channels.forEach(channel => {
-        let logoUrl = '';
-        if (channel.logo) {
-          // 处理相对路径：将 ./data/logo/xxx.png 映射为 URL /data/logo/xxx.png
-          if (channel.logo.startsWith('./')) {
-            const cleanPath = channel.logo.replace('./', '/');
-            logoUrl = `${baseUrl}${cleanPath}`;
-          } else {
-            logoUrl = `https://fy.188766.xyz/logo/fanmingming/live/tv/${channel.logo}.png`;
-          }
+      group.channels.forEach(c => {
+        // Logo处理: ./data/xxx -> https://domain/data/xxx
+        // 远程Logo保持不变
+        let logo = c.logo || '';
+        if (logo.startsWith('./')) {
+          logo = `${baseUrl}${logo.substring(1)}`;
+        } else if (!logo.startsWith('http')) {
+          logo = `https://fy.188766.xyz/logo/fanmingming/live/tv/${logo}.png`;
         }
-
-        m3uContent += `#EXTINF:-1 tvg-name="${channel.name}" tvg-logo="${logoUrl}" group-title="${group.group}",${channel.name}\n`;
-        m3uContent += `${iptvBaseUrl}${channel.id}\n`;
+        content += `#EXTINF:-1 tvg-name="${c.name}" tvg-logo="${logo}" group-title="${group.group}",${c.name}\n${iptvUrl}${c.id}\n`;
       });
     });
-
     res.setHeader('Content-Type', 'audio/x-mpegurl; charset=utf-8');
-    res.setHeader('Content-Disposition', 'inline; filename="ipv6.m3u"');
-    return res.send(m3uContent);
-  } 
-  
-  else if (type === 'txt') {
-    let txtContent = '';
-
-    data.forEach((group, index) => {
-      txtContent += `${group.group},#genre#\n`;
-      
-      group.channels.forEach(channel => {
-        txtContent += `${channel.name},${iptvBaseUrl}${channel.id}\n`;
-      });
-
-      if (index < data.length - 1) {
-        txtContent += '\n';
-      }
+    res.send(content);
+  } else if (type === 'txt') {
+    let content = '';
+    data.forEach((group, i) => {
+      content += `${group.group},#genre#\n`;
+      group.channels.forEach(c => content += `${c.name},${iptvUrl}${c.id}\n`);
+      if (i < data.length - 1) content += '\n';
     });
-
     res.setHeader('Content-Type', 'text/plain; charset=utf-8');
-    return res.send(txtContent);
-  }
-
-  else {
-    res.status(400).send('Invalid type');
+    res.send(content);
+  } else {
+    res.status(400).send('Bad Request');
   }
 }
